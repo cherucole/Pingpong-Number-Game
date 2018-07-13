427f5fbcf698957e783bef79d4ff3103b7eff40c
cherucole <cheruyotcheruyot@gmail.com>
2018-07-13 15:55:32 +0300

    Made content responsive to mobile device

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 
+// User Interface Logic
 $(document).ready(function() {
   $("form#myForm").submit(function(event) {
     event.preventDefault()
     var userNumber = parseInt($("input#userInput").val())
+    console.log(userNumber);
 
+
+// Business Logic
     for (var i = 1; i <= userNumber; i++) {
       var result = "";
       if (i % 3 != 0 && i % 5 != 0) result =(i)
       if (i % 3 === 0) result = "ping"
       if (i % 5 === 0) result += "pong"
-
-
 
       $("p#display").append(result + "</br>")
     }
