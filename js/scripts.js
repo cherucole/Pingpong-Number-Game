f1689f9668ebab407d3321783cf40d6c40b91b41
cherucole <cheruyotcheruyot@gmail.com>
2018-07-13 12:32:32 +0300

    list of numbers showing in html

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,9 @@ $(document).ready(function() {
     for (var i = 1; i <= userNumber; i++) {
       var result = "";
       if (i % 3 === 0) result = "ping";
-      if (i % 5 === 0) result += "pong"
-      console.log(result || i);
+      if (i % 5 === 0) result += "pong";
+      $("p#display").append(result || i+"</br>")
     }
 
-    $("p.display").text(result || i)
   });
 });
