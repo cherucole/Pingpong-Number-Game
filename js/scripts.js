ec3f81deebd67b8138fd4746ddb6d167e97916ae
cherucole <cheruyotcheruyot@gmail.com>
2018-07-13 12:52:43 +0300

    logic corrected

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,10 @@ $(document).ready(function() {
 
     for (var i = 1; i <= userNumber; i++) {
       var result = "";
-      if (i % 3 === 0) result = "ping";
-      if (i % 5 === 0) result += "pong";
-      $("p#display").append(result || i+"</br>")
+      if (i % 3 != 0 && i % 5 != 0) result =(i + "<br>")
+      if (i % 3 === 0) result = "ping"
+      if (i % 5 === 0) result += "pong"
+      $("p#display").append(result + "</br>"|| i+ "</br>")
     }
 
   });
