a612e3e9a49404993c2eb02fbdb13d0f219f60c1
cherucole <cheruyotcheruyot@gmail.com>
2018-07-13 13:01:54 +0300

    code corrected and working as expected

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     for (var i = 1; i <= userNumber; i++) {
       var result = "";
-      if (i % 3 != 0 && i % 5 != 0) result =(i + "<br>")
+      if (i % 3 != 0 && i % 5 != 0) result =(i)
       if (i % 3 === 0) result = "ping"
       if (i % 5 === 0) result += "pong"
       $("p#display").append(result + "</br>"|| i+ "</br>")
