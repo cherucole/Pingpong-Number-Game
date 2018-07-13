ffa5197522ae1f2ab12e00e5f5be0a8d0109ca39
cherucole <cheruyotcheruyot@gmail.com>
2018-07-13 11:23:46 +0300

    linked forms and html display

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("form#myForm").submit(function(event){
-    event.preventdefault()
-    var
-  })
-})
+    event.preventDefault()
+    var userNumber=parseInt($("input#userInput").val())
+    $("p.display").text(userNumber)
+  });
+});
