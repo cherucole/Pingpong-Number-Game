7ba8e8b4dbae820841d1623f785034f6f3b2848d
cherucole <cheruyotcheruyot@gmail.com>
2018-07-13 17:16:11 +0300

    Enabled clear output before another input

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 // User Interface Logic
 $(document).ready(function() {
   $("form#myForm").submit(function(event) {
@@ -6,11 +5,13 @@ $(document).ready(function() {
     var userNumber = parseInt($("input#userInput").val())
     console.log(userNumber);
 
+// Ensuring page clears before new input
+    $("#display").empty();
 
-// Business Logic
+    // Business Logic
     for (var i = 1; i <= userNumber; i++) {
       var result = "";
-      if (i % 3 != 0 && i % 5 != 0) result =(i)
+      if (i % 3 != 0 && i % 5 != 0) result = (i)
       if (i % 3 === 0) result = "ping"
       if (i % 5 === 0) result += "pong"
 
